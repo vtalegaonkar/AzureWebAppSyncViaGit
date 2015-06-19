f6cdda8cda0933668ad6a7ff42d31cecc2261775
Vrushal Talegaonkar <vrushal.hi@gmail.com>
2015-06-19 17:06:55 +0530

    InitialCommit

---
+++ AzureWebAppSyncViaGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebAppSyncViaGit.Startup))]
+namespace AzureWebAppSyncViaGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
